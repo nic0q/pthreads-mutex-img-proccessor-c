c625c6ad9f574294b6028c5799e63223a2007ad3
John Serrano <91446330+PodssilDev@users.noreply.github.com>
2022-06-22 00:00:21 -0400

    Add files via upload

--- funciones.c
+++ funciones.c
@@ -4,6 +4,21 @@ FILE* f;  // Descriptor global
 
 float originDistance(float u, float v) { return sqrt(pow(u, 2) + pow(v, 2)); }
 
+int getIndexProccess(int numeroDiscos, int anchoDiscos, float oDistance) {
+  int j = anchoDiscos;
+  if(oDistance >= (numeroDiscos - 1) * anchoDiscos) {
+    return numeroDiscos - 1;
+  }
+  else{
+    for(int i = 0;i < numeroDiscos;i++) {
+      if(i * anchoDiscos <= oDistance && oDistance < j){
+        return i;
+      }
+      j = j + anchoDiscos;
+    }
+  }
+}
+
 void openFile(char* name) {
   f = fopen(name, "r");  // Se abre el archivo
   // printf("%d %d %d hola", numeroDiscos , anchoDiscos , chunksPorLeer);
@@ -12,25 +27,40 @@ void openFile(char* name) {
     exit(0);
   }
 }
-void readFile(int nLines) {
+void readFile() {
   char buffer[1000];
-  float u, v, vis[3];
+  float u, v, vis[chunksPorLeer][4];
+  int disco_actual;
   int cont = 0;
   while (fgets(buffer, 1000, f) != NULL) {
-    sscanf(buffer, "%f,%f,%f,%f,%f", &u, &v, &vis[0], &vis[1], &vis[2]);
+    sscanf(buffer, "%f,%f,%f,%f,%f", &u, &v, &vis[cont][1], &vis[cont][2], &vis[cont][3]);
     float oDistance = originDistance(u, v);  // Se calcula la distancia de la visibilidad al origen
     int index = getIndexProccess(numeroDiscos, anchoDiscos, oDistance);  // Se determina a que disco pertenece la visibilidad
-    if(cont == nLines){
-      return;
+    vis[cont][0] = index;
+    if(cont == chunksPorLeer){
+      break;
     }
     cont++;
+  }
+  for(int i = 0; i < 4; i++){
+    disco_actual = vis[i][0];
+    masterArray[disco_actual][0] += vis[i][1];
+    masterArray[disco_actual][1] += vis[i][2];
+    masterArray[disco_actual][2] += vis[i][3];
   }
 }
 
 void* trabajoHebras() {
+  int cont;
   while (1 == 1) {
-    pthread_mutex_lock(mutex);  // esto nos bloquea la SC
-    
+    pthread_mutex_lock(&mutex);  // esto nos bloquea la SC
+    cont += 1;
+    if(cont == 8){
+      pthread_mutex_unlock(&mutex);
+      pthread_exit(NULL);
+    }
+    readFile();
+    pthread_mutex_unlock(&mutex);
   }
 }
 // 2 discos , arreglo de 2 posiciones. Y cada posicion es sub arreglo que tiene
--- funciones.h
+++ funciones.h
@@ -4,8 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 int numeroDiscos, anchoDiscos, chunksPorLeer;
+#define CANT_DISCO (4)
+float media_real, media_imaginaria, potencia, ruido_total, masterArray[2][4];
 pthread_mutex_t mutex;
 
 void openFile(char* name);
 void* trabajoHebras();
 float originDistance(float u, float v);
+int getIndexProccess(int numeroDiscos, int anchoDiscos, float oDistance);
+void readfile();
